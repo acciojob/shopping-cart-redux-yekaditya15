0a5ff62dde46aac553ac055084632e472b55be6a
yekaditya11 <yekaditya11@gmail.com>
2025-02-02 12:23:42 +0530

    update1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
-
-import React from "react";
+import React from 'react';
 import './../styles/App.css';
-
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      
     </div>
   )
 }
